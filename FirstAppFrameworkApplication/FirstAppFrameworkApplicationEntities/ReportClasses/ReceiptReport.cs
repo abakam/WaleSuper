9a839066ec2c3271df02ea233e3fb1fe140522db
abakam <abrahamakam@gmail.com>
2016-02-02 15:48:49 +0100

    hnjbl
    kjlboh

---
+++ FirstAppFrameworkApplication/FirstAppFrameworkApplicationEntities/ReportClasses/ReceiptReport.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppFramework.AppClasses;
+using AppFramework.Controls;
+
+namespace FirstAppFrameworkApplicationEntities.ReportClasses
+{
+    class ReceiptReport : MicrosoftReportViewerReportRun
+    {
+        public override MicrosoftReportViewerReportType ReportType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override void initDataSources()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void postInitDatasources()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void postInitReport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string reportPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Title
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
